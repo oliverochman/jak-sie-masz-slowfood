a797cbd67d6551b40f79cbf94b035e5e918694d1
aljoh <reachingalfred@gmail.com>
2018-01-16 16:39:29 +0100

    refactors features env file

--- features/support/env.rb
+++ features/support/env.rb
@@ -17,19 +17,6 @@ end
 
 Capybara.javascript_driver = :chrome
 
-#Capybara.register_driver :selenium do |app|
-  #options = Selenium::WebDriver::Chrome::Options.new(
-  #  args: %w[headless disable-popup-blocking]
-  #)
-
-  #Capybara::Selenium::Driver.new(
-    #app,
-    #browser: :chrome,
-    #options: options
-  #)
-#end
-
-#Capybara.javascript_driver = :selenium
 ActionController::Base.allow_rescue = false
 
 begin
